7b2a8df40ef3aeb4dc51c8e85cb4b68a4fe83a6f
jcdavidarg <jcdavidarg@gmail.com>
2024-07-01 17:56:03 -0300

    primer commit archivos base para chat bidireccional de websocket

---
+++ server/index.js
@@ -0,0 +1,21 @@
+//  IMPORTO DEPENDENCIAS DE NODE
+import express from 'express';  
+import logger from 'morgan';
+
+//  ESTABLESCO EL PUERTO
+const port = process.env.PORT ?? 3000;
+
+//  ESTABLESCO EL SSERVIDOR CON EXPRESS
+const app = express();
+//  QUE EL SERVIDOR USO EL LOGGER
+app.use(logger('dev'));
+
+//  CUANDO RECIBA UN GET QUE HAGO ESTO
+app.get('/', (req, res) => {
+    res.sendFile(process.cwd() + '/client/index.html');
+});
+
+//  ARRANCO EL SERVIDOR PARA QUE ESCUCHE EN EL PUERTO
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
