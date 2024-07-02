43b1942049b6aac634787b23d23698204153e52a
jcdavidarg <jcdavidarg@gmail.com>
2024-07-02 01:45:57 -0300

    integrando servidor http, express y socket.io

--- server/index.js
+++ server/index.js
@@ -1,21 +1,32 @@
 //  IMPORTO DEPENDENCIAS DE NODE
-import express from 'express';  
-import logger from 'morgan';
+import express from "express";
+import logger from "morgan";
+
+import { Server } from "socket.io";
+import { createServer } from "node:http";
 
 //  ESTABLESCO EL PUERTO
 const port = process.env.PORT ?? 3000;
 
-//  ESTABLESCO EL SERVIDOR CON EXPRESS
+
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+//  CUANDO SE CONECTE AL SOCKET IO
+io.on("connection", () => {
+  console.log("A user has connected!");
+});
+
 //  QUE EL SERVIDOR USO EL LOGGER
-app.use(logger('dev'));
+app.use(logger("dev"));
 
 //  CUANDO RECIBA UN GET QUE HAGO ESTO
-app.get('/', (req, res) => {
-    res.sendFile(process.cwd() + '/client/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(process.cwd() + "/client/index.html");
 });
 
 //  ARRANCO EL SERVIDOR PARA QUE ESCUCHE EN EL PUERTO
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
